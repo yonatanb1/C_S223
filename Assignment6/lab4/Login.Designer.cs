9842b309d335957f8d2e978c8fb86da5e341fc1a
yonatanb1 <yonatanb7237@gmail.com>
2022-08-16 23:44:49 +0300

    Login page, check boxes, radio button

---
+++ Assignment6/lab4/Login.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace lab4
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.passwdLabel = new System.Windows.Forms.Label();
+            this.passwdTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(218, 150);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(44, 16);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(330, 147);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(184, 22);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // passwdLabel
+            // 
+            this.passwdLabel.AutoSize = true;
+            this.passwdLabel.Location = new System.Drawing.Point(218, 195);
+            this.passwdLabel.Name = "passwdLabel";
+            this.passwdLabel.Size = new System.Drawing.Size(67, 16);
+            this.passwdLabel.TabIndex = 2;
+            this.passwdLabel.Text = "Password";
+            // 
+            // passwdTextBox
+            // 
+            this.passwdTextBox.Location = new System.Drawing.Point(330, 195);
+            this.passwdTextBox.Name = "passwdTextBox";
+            this.passwdTextBox.Size = new System.Drawing.Size(184, 22);
+            this.passwdTextBox.TabIndex = 3;
+            this.passwdTextBox.UseSystemPasswordChar = true;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.IndianRed;
+            this.loginButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.loginButton.Location = new System.Drawing.Point(439, 247);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 25);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwdTextBox);
+            this.Controls.Add(this.passwdLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "Login";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label passwdLabel;
+        private System.Windows.Forms.TextBox passwdTextBox;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
